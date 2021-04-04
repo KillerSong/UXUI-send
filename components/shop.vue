a744ef48717b8746d0c70ae138c08774c2f28bb0
KillerSong <waihou77777@gmail.com>
2021-04-04 23:30:32 +0700

    New

---
+++ components/shop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex bg-green-500 w-18 p-4 rounded-full">
+      <img class="h-8 w-8" src="https://e7.pngegg.com/pngimages/833/426/png-clipart-black-shopping-cart-icon-for-free-black-shopping-cart.png" alt="">
+      <h1>{{count}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        count:{
+            default:0
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
